168d4cb1ff4e0a0abab8e6cf625c1676f8747ed2
bp-rwidianto <resha-dirga.widianto@brainproducts.com>
2025-07-08 10:39:04 +0200

    Update instructions in 09_📃_Instruction_of_Use.py

--- pages/09_📃_Instruction_of_Use.py
+++ pages/09_📃_Instruction_of_Use.py
@@ -12,3 +12,14 @@ st.write(
     and created a network to see the relationship between authors.
     """
 )
+
+st.write("""
+    To use this dashboard, you need to first upload the files in 🏠 Home page (first page opened), which you can access from the sidebar on the left:
+
+        - Network Edges file: containing the relationships of authors
+        - Node Information file: containing information snippet of authors that can be used for filtering
+
+    _###Please reach out to Business Development team to get the latest version of these files###_
+
+    We recommend to use both Network analysis window and the Author Publication Journey to get a full context of your analysis. The Author Publication Journey is only accessible if you open this dashboard from Zoho Analytics.
+""")
